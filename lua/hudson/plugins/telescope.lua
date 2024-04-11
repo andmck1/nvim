532dc3cd69a9598d2ac2a2d421ab045f44bc01e2
Alastair Hamilton <alastair@altruistiq.com>
2024-04-11 15:01:22 +0100

    chore: reorganised telescope dependencies

--- lua/hudson/plugins/telescope.lua
+++ lua/hudson/plugins/telescope.lua
@@ -1,3 +1,17 @@
 return {
-    "nvim-telescope/telescope.nvim"
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+	    "nvim-tree/nvim-web-devicons",
+	    {
+		"nvim-treesitter/nvim-treesitter",
+		build = function()
+			        require("nvim-treesitter.install").update({ with_sync = true })()
+			end
+	    },
+	    "nvim-lua/plenary.nvim",
+	    {
+		    "nvim-telescope/telescope-fzf-native.nvim",
+		    build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build"
+	    }
+    }
 }
