b3c8f5e1d0bfa79085199ff318743b1295791973
Alastair Hamilton <alastair.amhamilton@icloud.com>
2024-04-13 21:23:41 +0100

    feat: updated telescope settings to show hidden and ignore certain files

--- lua/hudson/plugins/telescope.lua
+++ lua/hudson/plugins/telescope.lua
@@ -14,32 +14,43 @@ return {
 			build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
 			config = function()
 				require'nvim-treesitter.configs'.setup {
-					-- A list of parser names, or "all" (the five listed parsers should always be installed)
-					ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "typescript", "python", "terraform" },
+					ensure_installed = {
+                        "c", "lua", "vim", "vimdoc", "query", "javascript",
+                        "typescript", "python", "terraform", "yaml"
+                    },
 
-					-- Install parsers synchronously (only applied to `ensure_installed`)
-				    	sync_install = false,
+                    sync_install = true,
 
-					-- Automatically install missing parsers when entering buffer
-					-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
 					auto_install = true,
 
 					highlight = {
 						enable = true,
-
-						additional_vim_regex_highlighting = false,
 					},
 				}
 			end
 	    }
     },
     config = function()
+        local telescope = require("telescope")
 	    local builtin = require('telescope.builtin')
 	    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 	    vim.keymap.set("n", "<leader>ps",
 	    function()
 		    builtin.grep_string({ search = vim.fn.input("Grep > ") });
 	    end)
+
+        telescope.setup {
+            pickers = {
+                find_files = {
+                    hidden = true
+                }
+            },
+            defaults = {
+                file_ignore_patterns = {
+                    ".git/", ".data", ".DS_Store"
+                }
+            }
+        }
     end
 
 }
