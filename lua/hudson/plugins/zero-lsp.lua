8f0660abcf29738ce287eedba4a449fbcbd4fff3
andmck <alastair.amhamilton@icloud.com>
2024-04-22 09:14:15 +0100

    build: remove mason

--- lua/hudson/plugins/zero-lsp.lua
+++ lua/hudson/plugins/zero-lsp.lua
@@ -1,27 +1,39 @@
 return {
-	'williamboman/mason.nvim',
-	'williamboman/mason-lspconfig.nvim',
 	{
 		"VonHeikemen/lsp-zero.nvim",
 		config = function()
 			local lsp_zero = require('lsp-zero')
+            local lsp_config = require("lspconfig")
 
 			lsp_zero.on_attach(function(client, bufnr)
 				lsp_zero.default_keymaps({buffer = bufnr})
 			end)
-			require('mason').setup({})
-			require('mason-lspconfig').setup({
-				ensure_installed = {},
-				handlers = {
-					function(server_name)
-						require('lspconfig')[server_name].setup({})
-					end,
-				},
-			})
 
-		end,
+            lsp_config.pyright.setup({
+                  settings = {
+                      pyright = {
+                          disableOrganizeImports = true,
+                      },
+                      python = {
+                          analysis = {
+                              ignore = { '*' },
+                          },
+                      },
+                  },
+            })
+            lsp_config.ruff.setup({})
+            lsp_config.terraformls.setup({})
+        end,
+        dependencies = {
+            'neovim/nvim-lspconfig', 
+        },
+        build = [[
+        pip install ruff &&
+        pip install pyright
+        ]]
+        
+
 	},
-	'neovim/nvim-lspconfig',
 	'hrsh7th/cmp-nvim-lsp',
 	'hrsh7th/nvim-cmp',
 	'L3MON4D3/LuaSnip',
