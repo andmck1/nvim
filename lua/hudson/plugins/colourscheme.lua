e2c35087dfbe930b24b7b4481f5734672e4fec2e
Alastair Hamilton <alastair.amhamilton@icloud.com>
2024-05-10 12:38:09 +0100

    chore: removed whitespace

--- lua/hudson/plugins/colourscheme.lua
+++ lua/hudson/plugins/colourscheme.lua
@@ -1,8 +1,8 @@
 return {
 	{
-		"bluz71/vim-moonfly-colors", 
-		name = "moonfly", 
-		lazy = false, 
+		"bluz71/vim-moonfly-colors",
+		name = "moonfly",
+		lazy = false,
 		priority = 100,
 		config = function()
 			vim.cmd("colorscheme moonfly")
