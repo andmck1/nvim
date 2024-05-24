05d08458e0df8ddf199cb3a5adb817b5ff9e0403
andmck <alastair.amhamilton@icloud.com>
2024-05-24 11:41:44 +0100

    feat: added latex

--- lua/hudson/plugins/zero-lsp.lua
+++ lua/hudson/plugins/zero-lsp.lua
@@ -1,15 +1,15 @@
 return {
-	'williamboman/mason.nvim',
-	'williamboman/mason-lspconfig.nvim',
-	{
-		"VonHeikemen/lsp-zero.nvim",
-		config = function()
-			local lsp_zero = require('lsp-zero')
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
+    {
+        "VonHeikemen/lsp-zero.nvim",
+        config = function()
+            local lsp_zero = require('lsp-zero')
             local cmp = require("cmp")
             lsp_zero.extend_lspconfig()
 
             lsp_zero.on_attach(function(client, bufnr)
-                lsp_zero.default_keymaps({buffer = bufnr})
+                lsp_zero.default_keymaps({ buffer = bufnr })
             end)
 
             cmp.setup({
@@ -17,26 +17,31 @@ return {
                     ['<C-k>'] = cmp.mapping.select_prev_item(),
                     ['<C-j>'] = cmp.mapping.select_next_item(),
                     ['<C-u>'] = lsp_zero.cmp_action().toggle_completion(),
-                    ['<Tab>'] = cmp.mapping.confirm({select = true}),
+                    ['<Tab>'] = cmp.mapping.confirm({ select = true }),
                 }
             })
             vim.keymap.set('n', '<leader>r', vim.lsp.buf.format)
 
-			require('mason').setup({})
-			require('mason-lspconfig').setup({
-				ensure_installed = {"ruff_lsp", "pyright", "lua_ls", "bashls", "yamlls"},
-				handlers = {
+            require('mason').setup({})
+            require('mason-lspconfig').setup({
+                ensure_installed = {
+                    "ruff_lsp",
+                    "pyright",
+                    "lua_ls",
+                    "bashls",
+                    "yamlls",
+                    "texlab"
+                },
+                handlers = {
                     function(server_name)
                         require('lspconfig')[server_name].setup({})
                     end,
                 },
             })
-
-		end,
-	},
-	'neovim/nvim-lspconfig',
-	'hrsh7th/cmp-nvim-lsp',
-	'hrsh7th/nvim-cmp',
-	'L3MON4D3/LuaSnip',
+        end,
+    },
+    'neovim/nvim-lspconfig',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/nvim-cmp',
+    'L3MON4D3/LuaSnip',
 }
-
