abc5cb0f370ef35258a7c28255694fbcc4448436
Alastair Hamilton <alastair@altruistiq.com>
2025-01-27 15:21:02 +0000

    feat: added keymap for code actions

--- lua/hudson/core/keymaps.lua
+++ lua/hudson/core/keymaps.lua
@@ -30,4 +30,5 @@ vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
-
+--- lsp code actions
+vim.keymap.set("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>")
