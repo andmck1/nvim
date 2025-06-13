432c37ede5d3cb4ab60e6fcc5efe4fa8345486e0
andmck <alastair.amhamilton@icloud.com>
2025-06-13 22:51:44 +0100

    feat: updated colour scheme

--- lua/hudson/postsetup.lua
+++ lua/hudson/postsetup.lua
@@ -1,4 +1,7 @@
 -- colourscheme
-vim.cmd("colorscheme bamboo")
+vim.cmd("colorscheme kanagawa")
+vim.cmd("highlight NonText guifg=white")
 vim.cmd("highlight Normal guibg=black guifg=white")
+vim.cmd("highlight LineNr guibg=black guifg=white")
+vim.cmd("highlight SignColumn guibg=black guifg=white")
 vim.cmd("highlight ColorColumn guibg=DarkRed")
