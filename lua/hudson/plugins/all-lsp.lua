366099ad1c59198f34c74050e9398a96edee17f7
Alastair Hamilton <alastair.amhamilton@icloud.com>
2025-10-21 11:13:16 +0100

    fix: updated lsps

--- lua/hudson/plugins/all-lsp.lua
+++ lua/hudson/plugins/all-lsp.lua
@@ -2,21 +2,7 @@ return {
     {
         'neovim/nvim-lspconfig',
         config = function()
-            local lspconfig = require("lspconfig")
-            local lspconfig_defaults = require('lspconfig').util.default_config
-
-            lspconfig_defaults.capabilities = vim.tbl_deep_extend(
-                'force',
-                lspconfig_defaults.capabilities,
-                require('cmp_nvim_lsp').default_capabilities()
-            )
-            lspconfig_defaults.capabilities = vim.tbl_deep_extend(
-                'force',
-                lspconfig_defaults.capabilities,
-                require('blink.cmp').get_lsp_capabilities({}, false)
-            )
-
-            lspconfig['clangd'].setup {
+            vim.lsp.config('clangd', {
                 cmd = {
                     "clangd",
                     "--background-index",
@@ -25,89 +11,7 @@ return {
                     "--limit-results=50",
                 },
             }
-
-            require('mason').setup({})
-            require('mason-lspconfig').setup({
-                ensure_installed = {
-                    "ruff",
-                    "pyright",
-                    "lua_ls",
-                    "bashls",
-                    "yamlls",
-                    "texlab",
-                    "terraformls",
-                },
-                handlers = {
-                    function(server_name)
-                        local capabilities = require('blink.cmp').get_lsp_capabilities()
-                        lspconfig[server_name].setup({
-                            capabilities = capabilities,
-                        })
-                    end,
-
-                    ["ruff"] = function()
-                        local on_attach = function(client, _)
-                            if client.name == 'ruff' then
-                                client.server_capabilities.hoverProvider = false
-                            end
-                        end
-                        lspconfig.ruff.setup({
-                            on_attach = on_attach,
-                        })
-                    end,
-
-                    ["pyright"] = function()
-                        lspconfig.pyright.setup({
-                            settings = {
-                                pyright = {
-                                    disableOrganizeImports = true,
-                                },
-                                python = {
-                                },
-                            },
-                        })
-                    end,
-
-                    ["yamlls"] = function()
-                        lspconfig.yamlls.setup {
-                            settings = {
-                                yaml = {
-                                    format = {
-                                        enable = true
-                                    },
-                                    schemaStore = {
-                                        enable = true
-                                    }
-                                }
-                            }
-                        }
-                    end,
-
-                    ["terraformls"] = function()
-                        local lspconfig = require("lspconfig")
-                        lspconfig.terraformls.setup {
-                            filetypes = {
-                                "terraform", "terraform-vars"
-                            }
-                        }
-                    end,
-
-                    ["lua_ls"] = function()
-                        local lspconfig = require("lspconfig")
-                        lspconfig.lua_ls.setup {
-                            settings = {
-                                lua = {
-                                    diagnostics = {
-                                        globals = {
-                                            "vim"
-                                        }
-                                    },
-                                }
-                            }
-                        }
-                    end,
-                },
-            })
+            )
 
             local cmp = require('cmp')
 
@@ -125,8 +29,14 @@ return {
         dependencies = {
             'hrsh7th/cmp-nvim-lsp',
             'hrsh7th/nvim-cmp',
-            'williamboman/mason.nvim',
-            'williamboman/mason-lspconfig.nvim',
+            {
+                "mason-org/mason-lspconfig.nvim",
+                opts = {},
+                dependencies = {
+                    { "mason-org/mason.nvim", opts = {} },
+                    "neovim/nvim-lspconfig",
+                },
+            }
         },
     },
 }
