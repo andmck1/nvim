ce3be78c94e00a14005c0373a12fcbb6f8d8a019
Alastair Hamilton <alastair.amhamilton@icloud.com>
2025-11-23 19:47:58 +0000

    feat: cleaned up git

--- lua/hudson/plugins/snacks.lua
+++ lua/hudson/plugins/snacks.lua
@@ -12,16 +12,23 @@ return {
                         "build/",
                         ".cache",
                         "assets",
+                        "__pycache__",
+                        ".ipynb_checkpoints"
                     },
                 },
             },
-        }
+        },
+        lazygit = {},
+        gitbrowse = {},
     },
     keys = {
         { "<leader>fs", function() Snacks.picker.smart() end,     desc = "Smart Find Files" },
         { "<leader>ff", function() Snacks.picker.files() end,     desc = "Find Files" },
         { "<leader>f/", function() Snacks.picker.grep() end,      desc = "Grep" },
         { "<leader>fg", function() Snacks.picker.git_files() end, desc = "Find Git Files" },
+        { "<leader>gg", function() Snacks.lazygit.open() end,     desc = "Open Lazygit" },
+        { "<leader>gb", function() Snacks.gitbrowse.open() end,   desc = "Open File in Github" },
+        { "<leader>gd", function() Snacks.picker.git_diff() end,  desc = "Open Git Diff of File" }
     },
     dependencies = {
         "nvim-tree/nvim-web-devicons",
