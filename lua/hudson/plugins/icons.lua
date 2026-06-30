01e71a40c5a7c5961dfda0e8dc3008914399ede7
Alastair Hamilton <alastair.amhamilton@icloud.com>
2026-06-30 10:27:22 +0100

    feat: added slang icon

--- lua/hudson/plugins/icons.lua
+++ lua/hudson/plugins/icons.lua
@@ -2,4 +2,20 @@ return {
 	"nvim-tree/nvim-web-devicons",
 	lazy = false,
 	priority = 99999999,
+	config = function()
+		require("nvim-web-devicons").setup({
+			override_by_extension = {
+				slang = {
+					icon = "",
+                    color = "#fd6533",
+					name = "Slang",
+				},
+				slangh = {
+					icon = "",
+                    color = "#fd6533",
+					name = "Slang",
+				},
+			},
+		})
+	end,
 }
